cfd2cef6fd1c80ecc60d333570244ca56af5378f
KevinBeltran23 <kevinbeltran2437@gmail.com>
2025-11-04 16:52:08 -0800

    added to post notifs to tiramisu and above

--- app/src/main/java/com/zybooks/countdowntimer/MainActivity.kt
+++ app/src/main/java/com/zybooks/countdowntimer/MainActivity.kt
@@ -21,12 +21,20 @@ import com.zybooks.countdowntimer.ui.TimerScreen
 import com.zybooks.countdowntimer.ui.TimerViewModel
 import com.zybooks.countdowntimer.ui.theme.CountdownTimerTheme
 import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 
 class MainActivity : ComponentActivity() {
 
    private val timerViewModel = TimerViewModel()
+   private val permissionRequestLauncher =
+      registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+         val message = if (isGranted) "Permission granted" else "Permission NOT granted"
+         Log.i("MainActivity", message)
+      }
 
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
@@ -41,6 +49,14 @@ class MainActivity : ComponentActivity() {
             }
          }
       }
+      // Only need permission to post notifications on Tiramisu and above
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+         if (ActivityCompat.checkSelfPermission(this,
+               Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_DENIED) {
+            permissionRequestLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+         }
+      }
+
    }
 
    override fun onStop() {
